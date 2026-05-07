c8f27c5ef539b0d8fce3b8d2b212726c63b50244
QiuYe <qiuye2024github@163.com>
2026-05-07 11:40:33 +0800

    修复一些未预期行为

---
+++ src/main/java/com/modularmc/ten/core/mixin/dev/client/KeyboardHandlerMixin.java
@@ -0,0 +1,8 @@
+package com.modularmc.ten.core.mixin.dev.client;
+
+import net.minecraft.client.KeyboardHandler;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(KeyboardHandler.class)
+public class KeyboardHandlerMixin {
+}
